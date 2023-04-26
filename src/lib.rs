bced46951dd554fa299b22c953d3215776e69bd4
xd009642 <danielmckenna93@gmail.com>
2023-04-26 16:09:09 +0000

    Merged in feat/fancy-rewriting-api (pull request #20)
    Feat/fancy rewriting api
    
    * Fancy rewriting api thing
    
    * Merge branch 'master' into feat/fancy-rewriting-api
    
    * Add a test and fix escaping
    
    * Attempt to make async friendly
    
    * Change async design a lot
    
    * Async feature
    
    * Fix test
    
    * Comment things
    
    
    Approved-by: Davi Pereira
    Approved-by: Salil Deena

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
-use elements::ParsedElement;
-
 use crate::{elements::SsmlElement, parser::Span};
+use elements::ParsedElement;
+use std::fmt;
+use std::ops::FnMut;
 /// Valid SSML:
 ///
 /// ```xml
@@ -20,11 +21,22 @@ use crate::{elements::SsmlElement, parser::Span};
 pub mod elements;
 pub mod parser;
 
+/// Holds parsed SSML string with the text minus tags and the tag information
 #[derive(Clone, Debug)]
 pub struct Ssml {
     text: String,
     pub(crate) tags: Vec<Span>,
     pub(crate) event_log: ParserLog,
+}
+
+/// After applying a transformation to SSML writes out the new SSML string and also the
+/// text to be processed by a speech synthesiser. Assumes all text in custom tags is synthesisable.
+#[derive(Clone, Debug)]
+pub struct TransformedSsml {
+    /// Generated SSML String after transformation
+    pub ssml_string: String,
+    /// Synthesisable text after the transformation
+    pub synthesisable_text: String,
 }
 
 type ParserLog = Vec<ParserLogEvent>;
@@ -35,6 +47,45 @@ pub(crate) enum ParserLogEvent {
     Open(ParsedElement),
     Close(ParsedElement),
     Empty(ParsedElement),
+}
+
+#[derive(Clone, Debug)]
+pub enum ParserEvent {
+    Text(String),
+    Open(ParsedElement),
+    Close(ParsedElement),
+    Empty(ParsedElement),
+}
+
+/// This trait defines a function used to transform the ssml when asynchronous operations are
+/// involved.
+#[cfg(feature = "async")]
+#[async_trait::async_trait]
+pub trait AsyncSsmlTransformer {
+    /// Can be thought of as an asynchronous filter_map. Given a `ParserEvent` it will either
+    /// return a `ParserEvent` to be inserted into the stream or a `None` to remove the event from
+    /// the event stream. Self is mutable to allow for internal tracking of values.
+    async fn apply(&mut self, event: ParserEvent) -> Option<ParserEvent>;
+}
+
+impl fmt::Display for ParserEvent {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Text(text) => write!(f, "{}", quick_xml::escape::escape(&text)),
+            Self::Open(element) => {
+                let name: SsmlElement = element.into();
+                write!(f, "<{}{}>", name, element.attribute_string())
+            }
+            Self::Close(element) => {
+                let name: SsmlElement = element.into();
+                write!(f, "</{}>", name)
+            }
+            Self::Empty(element) => {
+                let name: SsmlElement = element.into();
+                write!(f, "<{}{}/>", name, element.attribute_string())
+            }
+        }
+    }
 }
 
 impl Ssml {
@@ -78,10 +129,89 @@ impl Ssml {
 
         ssml_str
     }
+
+    /// For each parser event to write out apply a transformation to it or return None if it should
+    /// be filtered out. It is up to the implementor to make sure that if an open tag is removed
+    /// the corresponding close tag is removed as well.
+    ///
+    /// TODO this doesn't track if there are tags where inner text shouldn't be synthesised so
+    /// certain transformations will lead to synthesisable_text being incorrect.
+    pub fn write_ssml_with_transform<F>(&self, mut f: F) -> TransformedSsml
+    where
+        F: FnMut(ParserEvent) -> Option<ParserEvent>,
+    {
+        let mut ssml_string = String::new();
+        let mut synthesisable_text = String::new();
+
+        use ParserLogEvent::*;
+        for event in self.event_log.iter().cloned() {
+            let new_event = match event {
+                Text(span) => {
+                    let (start, end) = span;
+                    f(ParserEvent::Text(self.text[start..end].to_string()))
+                }
+                Open(element) => f(ParserEvent::Open(element)),
+                Close(element) => f(ParserEvent::Close(element)),
+                Empty(element) => f(ParserEvent::Empty(element)),
+            };
+            if let Some(new_event) = new_event {
+                let string = new_event.to_string();
+                ssml_string.push_str(&string);
+                if let ParserEvent::Text(t) = new_event {
+                    synthesisable_text.push_str(&t);
+                }
+            }
+        }
+        TransformedSsml {
+            ssml_string,
+            synthesisable_text,
+        }
+    }
+
+    /// For each parser event to write out apply a transformation to it or return None if it should
+    /// be filtered out. It is up to the implementor to make sure that if an open tag is removed
+    /// the corresponding close tag is removed as well.
+    ///
+    /// TODO this doesn't track if there are tags where inner text shouldn't be synthesised so
+    /// certain transformations will lead to synthesisable_text being incorrect.
+    #[cfg(feature = "async")]
+    pub async fn async_write_ssml_with_transform(
+        self,
+        mut f: impl AsyncSsmlTransformer,
+    ) -> TransformedSsml {
+        let mut ssml_string = String::new();
+        let mut synthesisable_text = String::new();
+
+        use ParserLogEvent::*;
+        for event in self.event_log.iter().cloned() {
+            let new_event = match event {
+                Text(span) => {
+                    let (start, end) = span;
+                    f.apply(ParserEvent::Text(self.text[start..end].to_string()))
+                        .await
+                }
+                Open(element) => f.apply(ParserEvent::Open(element)).await,
+                Close(element) => f.apply(ParserEvent::Close(element)).await,
+                Empty(element) => f.apply(ParserEvent::Empty(element)).await,
+            };
+            if let Some(new_event) = new_event {
+                let string = new_event.to_string();
+                ssml_string.push_str(&string);
+                if let ParserEvent::Text(t) = new_event {
+                    synthesisable_text.push_str(&t);
+                }
+            }
+        }
+        TransformedSsml {
+            ssml_string,
+            synthesisable_text,
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     use crate::parser::parse_ssml;
     use quick_xml::events::Event;
     use quick_xml::reader::Reader;
@@ -148,5 +278,56 @@ mod tests {
         println!("{}", rewritten_trimmed);
 
         assert!(&ssml == &rewritten_trimmed);
+    }
+
+    #[test]
+    fn ssml_transformation() {
+        let ssml = r#"
+        <speak>
+            <mstts:backgroundaudio fadein="string" fadeout="string" src="string" volume="string"/>
+            <voice name="string">
+                <break strength="medium" time="5s"/>
+                <emphasis level="reduced"/>
+                <lang xml:lang="string"/>
+                <lexicon uri="string" xml:id="some_id"/>
+                <mstts:express-as role="string" style="string" styledegree="value"/>
+                <p>Some speech! &amp; With correct escaping on text, hopefully. </p>
+                <phoneme ph="string" alphabet="string"/>
+                <prosody pitch="2.2Hz" contour="(0%,+20Hz) (10%,+30Hz) (40%,+10Hz)" range="-2Hz" rate="20%" volume="2dB"/>
+            </voice>
+        </speak>        
+        "#;
+
+        let ssml = parse_ssml(ssml).unwrap();
+        // Now here we want to strip away the mstts tags and replace some text to be said. And then
+        // we'll reparse and make sure things seem sane
+
+        let transform = |elem| match &elem {
+            ParserEvent::Open(element)
+            | ParserEvent::Close(element)
+            | ParserEvent::Empty(element) => {
+                if matches!(element, ParsedElement::Custom(_)) {
+                    None
+                } else {
+                    Some(elem)
+                }
+            }
+            ParserEvent::Text(txt) => {
+                let txt = txt.replace("hopefully", "definitely");
+                Some(ParserEvent::Text(txt))
+            }
+        };
+
+        let transformed = ssml.write_ssml_with_transform(transform);
+        assert_eq!(
+            transformed.synthesisable_text.trim(),
+            "Some speech! & With correct escaping on text, definitely."
+        );
+        assert!(!transformed.ssml_string.contains("mstts:backgroundaudio"));
+        assert!(!transformed.ssml_string.contains("mstts:express-as"));
+        assert!(transformed.ssml_string.contains("prosody"));
+
+        // and hopefully our ssml is still valid:
+        parse_ssml(&transformed.ssml_string).unwrap();
     }
 }
